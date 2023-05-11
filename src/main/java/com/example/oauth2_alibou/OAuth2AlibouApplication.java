596186db11f623f777c2354f249649830ed3589d
Wiziwax <donaldwisdomnengi@gmail.com>
2023-05-11 05:44:43 +0100

    INITIAL ADD AND COMMIT

---
+++ src/main/java/com/example/oauth2_alibou/OAuth2AlibouApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_alibou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AlibouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AlibouApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/oauth2_alibou/Security/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.oauth2_alibou.Security;
+
+public class SecurityConfig {
+}
